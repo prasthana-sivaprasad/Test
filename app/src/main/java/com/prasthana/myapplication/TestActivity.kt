65fbe126a023c30d17956e553b86101e0cbd1152
prasthana-sivaprasad <sivaprasad.subbisetty@prasthana.com>
2019-12-13 17:24:49 +0530

    Dummy

--- app/src/main/java/com/prasthana/myapplication/TestActivity.kt
+++ app/src/main/java/com/prasthana/myapplication/TestActivity.kt
@@ -8,5 +8,7 @@ class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+
+//Test commit
     }
 }
